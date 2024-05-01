add6e6d2d57401c89fa8accaebfd0eb52f99d6a4
渡邊丈洋 <take-watanabe@autobacs.com>
2024-05-01 15:24:53 +0900

    メニュー画面を作成
    ログイン用の画面を用意

--- src/main/java/com/example/webapp/controller/ToDoController.java
+++ src/main/java/com/example/webapp/controller/ToDoController.java
@@ -1,7 +1,5 @@
 package com.example.webapp.controller;
 
-import java.lang.ProcessBuilder.Redirect;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
