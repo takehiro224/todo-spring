8e3407c4bea1c4898ae11248a69a55b806ea2f51
渡邊丈洋 <take-watanabe@autobacs.com>
2024-05-01 10:46:47 +0900

    動作確認コードを追加
    Serviceを利用したデータの取得動作を確認

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.webapp.entity.ToDo;
-import com.example.webapp.repository.ToDoMapper;
+import com.example.webapp.service.ToDoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,35 +16,35 @@ public class WebappApplication {
     }
 
     /** DI */
-    private final ToDoMapper mapper;
+    private final ToDoService service;
 
     public void exe() {
         /** 全件検索 */
         System.out.println("=== 全件検索 ===");
-        for (ToDo todo: mapper.selectAll()) {
+        for (ToDo todo: service.findAllToDo()) {
             System.out.println(todo);
         }
         /** 1件検索 */
         System.out.println("=== 1件検索 ===");
-        System.out.println(mapper.selectById(1));
+        System.out.println(service.findByIdToDo(1));
         /** 登録 */
         ToDo todo = new ToDo();
         todo.setTodo("リポジトリのテスト");
         todo.setDetail("DBへの登録処理");
-        mapper.insert(todo);
+        service.insertToDo(todo);
         System.out.println("=== 登録確認 ===");
-        System.out.println(mapper.selectById(4));
+        System.out.println(service.findByIdToDo(4));
         /** 更新 */
-        ToDo target = mapper.selectById(4);
+        ToDo target = service.findByIdToDo(4);
         target.setTodo("リポジトリのテスト: 更新");
         target.setDetail("DBへの更新処理");
-        mapper.update(target);
+        service.updateToDo(target);
         System.out.println("=== 更新確認 ===");
-        System.out.println(mapper.selectById(4));
+        System.out.println(service.findByIdToDo(4));
         /** 削除 */
-        mapper.delete(4);
+        service.deleteToDo(4);
         System.out.println("=== 削除確認 ===");
-        for (ToDo row: mapper.selectAll()) {
+        for (ToDo row: service.findAllToDo()) {
             System.out.println(row);
         }
     }
